9fb38e6005b64192727d5d0c8689ac2b337efecb
juel <email.juel@gmail.com>
2020-11-28 15:00:05 -0500

    updated

--- JuelGitRepoTest/src/juelGitTestPack/JuelGitTest.java
+++ JuelGitRepoTest/src/juelGitTestPack/JuelGitTest.java
@@ -5,6 +5,7 @@ public class JuelGitTest {
 	public static void main(String[] args) {
 		System.out.println("Juel Here");
 		System.out.println("Just another line");
+		System.out.println("Updated!");
 	}
 
 }
